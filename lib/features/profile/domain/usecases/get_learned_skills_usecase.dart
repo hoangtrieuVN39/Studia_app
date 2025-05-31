d7b0723679d0f28a8fe7c10706cbeca46e091ef6
Trieu <hoangtrieuvn39@gmail.com>
2025-05-31 18:16:21 +0700

    add constants learned Threshold

--- lib/features/profile/domain/usecases/get_learned_skills_usecase.dart
+++ lib/features/profile/domain/usecases/get_learned_skills_usecase.dart
@@ -1,10 +1,11 @@
+import 'package:studia/core/core.dart';
 import 'package:studia/core/data/datasources/local/drift/database.dart';
 
 class GetLearnedSkillsUsecase {
   GetLearnedSkillsUsecase();
 
   Map<Skills, bool> call(Map<Skills, Map<String, dynamic>> skills) {
-    final learnThreshold = 0.8;
+    final learnThreshold = AppMainConstants.learnedThreshold;
     final Map<Skills, bool> learnedSkills = skills.map(
       (key, value) => MapEntry(key, value['performance'] >= learnThreshold),
     );
